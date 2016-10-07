00dc70baacc670d019b28815e62dfc47d9ff6e0b
AlekseyPn <aleksfers@icloud.com>
2016-10-07 22:47:15 +0500

    developing JS for index page

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var popupOpen = document.querySelector(".company__btn--modal");
+var popup = document.querySelector(".modal-contents");
+var popupClose = document.querySelector(".modal-contents__close");
+var inputName = document.querySelector("[type='text']");
+var popupMap = document.querySelector(".modal-map");
+var mapOpen = document.querySelector(".company__map");
+var mapClose = document.querySelector(".modal-map__close");
+var overlay = document.querySelector(".modal-overlay");
+
+function openModal(click, popupVar, overlayVar) {
+    click.addEventListener("click", function(event) {
+        event.preventDefault();
+        popupVar.classList.add("modal-contents--show");
+        overlayVar.classList.add("modal-overlay--show");
+        popup.scrollIntoView(top);
+    })
+};
+function closeModal(click, popupVar, overlayVar) {
+    click.addEventListener("click", function(event) {
+        event.preventDefault();
+        popupVar.classList.remove("modal-contents--show");
+        overlayVar.classList.remove("modal-overlay--show");        
+    })
+};
+
+openModal(popupOpen, popup, overlay);
+openModal(mapOpen, popupMap, overlay);
+closeModal(popupClose, popup, overlay);
+closeModal(overlay, popup, overlay);
+closeModal(mapClose, popupMap, overlay);
+
+
+
+// popupOpen.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     popup.classList.add("modal-contents--show");
+//     overlay.classList.add("modal-overlay--show");
+//     inputName.focus();
+// });
